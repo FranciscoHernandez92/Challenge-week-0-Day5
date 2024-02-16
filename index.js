8f549aa75c5541107ce355976a737b86be0ad9a8
fran hernandez <franciscohernandezlopez5@gmail.com>
2024-02-16 19:18:49 +0100

    Add exer 1, 2, 3, 5, 6

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 //1.
-
+import { pyramidMakerWithAsterisk } from "./module.js";
+console.log(pyramidMakerWithAsterisk(5));
+console.log(pyramidMakerWithAsterisk(20));
 //2.
-
+import { passwordCreate } from "./module.js";
+console.log(passwordCreate(23));
+console.log(passwordCreate(5));
 //3.
-
+import { userNames } from "./module.js";
+console.log(userNames(['francisco', 'jose', 'carlos', 'cristobal', 'ines', 'heriberto', 'ana'], 4));
+console.log(userNames(['francisco', 'jose', 'carlos', 'cristobal', 'ines', 'heriberto', 'ana'], 8))
 //4.
 
 //5.
-
+import { countWords } from "./module.js";
+console.log(countWords('el perro ladra'));
+console.log(countWords('me encanta el bootcamp de isdi coders'));
 //6.
-
+import { orderNames } from "./module.js";
+console.log(orderNames(['bartolo', 'fran', 'carlos', 'dunia', 'ines', 'heriberto', 'ana']));
 //7.
 
 //8.
--- module.js
+++ module.js
@@ -1,15 +1,41 @@
 //1.
-
+export const pyramidMakerWithAsterisk = (size) =>{
+  let result = ''
+  for (let i = 1; i <= size; i++){
+    let space = ' '.repeat(size - i)
+    let asterisk = '*'.repeat(2 * i - 1)
+    result += space + asterisk + '\n'
+  } return result 
+}
 //2.
-
+export const passwordCreate = (size) => {
+  if (size <= 6){
+    return 'Your password needs a minimum of six characters'
+  }
+  let randomPassword = ''
+  const passwordOptions = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+  for (let i = 0; i < size; i++){
+    const randomIndex = Math.floor(Math.random() * passwordOptions.length)
+    randomPassword += passwordOptions[randomIndex]
+  }
+  return randomPassword
+}
 //3.
-
+export const userNames = (noun,length) => {
+  const filteredNames = noun.filter((i) => i.length < length)
+ return filteredNames
+}
 //4.
 
 //5.
-
+export const countWords = (str) => {
+  return str.trim().split(/\s+/).length
+}
 //6.
-
+export const orderNames = (alphabetized) => {
+  const sortedNames = [...alphabetized].sort()
+  return sortedNames
+} 
 //7.
 
 //8.
